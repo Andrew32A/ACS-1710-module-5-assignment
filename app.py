5e6ebe2de4e5f1192e1e8f488160b44c08c484cf
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-06 11:06:09 -0800

    finished create function and got database working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 ############################################################
@@ -8,8 +9,12 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+# mongo = PyMongo(app)
+
+client = MongoClient("localhost", 27017)
+plants = client.plants_database.plants
+harvests = client.plants_databse.harvests
 
 ############################################################
 # ROUTES
@@ -21,7 +26,7 @@ def plants_list():
 
     # TODO: Replace the following line with a database call to retrieve *all*
     # plants from the Mongo database's `plants` collection.
-    plants_data = ''
+    plants_data = plants.find()
 
     context = {
         'plants': plants_data,
@@ -40,16 +45,17 @@ def create():
         # TODO: Get the new plant's name, variety, photo, & date planted, and 
         # store them in the object below.
         new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
+            'name': request.form["plant_name"],
+            'variety': request.form["variety"],
+            'photo_url': request.form["photo"],
+            'date_planted': request.form["date_planted"]
         }
         # TODO: Make an `insert_one` database call to insert the object into the
         # database's `plants` collection, and get its inserted id. Pass the 
         # inserted id into the redirect call below.
+        insert_plant_id = plants.insert_one(new_plant).inserted_id
 
-        return redirect(url_for('detail', plant_id=''))
+        return redirect(url_for('detail', plant_id=insert_plant_id))
 
     else:
         return render_template('create.html')
